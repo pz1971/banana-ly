05e7066df6e8207314193af78ecd2025aef8642a
Parvez Mia <parvez.pz1971@gmail.com>
2021-04-06 01:30:48 +0600

    about page updated

--- public/about.php
+++ public/about.php
@@ -14,15 +14,18 @@
 <body>
     <header>
         <h1>banana-ly</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem distinctio optio eveniet commodi ducimus
-            dolorem, quia ratione quaerat obcaecati voluptate sint recusandae provident dicta sed earum unde corporis
-            omnis laboriosam?</p>
+        <p>
+            An enhanced url-shortener webpage, build with <3 as a group project for our university course (Software Development - IV)
+            Perhaps we're going to improve it later gethering more knowledges. In simple words- we take your long-ugly urls and shrink it
+            to small links and offer you a good protion of the income we earn from the ads we show during the redirection.
+        </p>
     </header>
     <div class="container" id="members">
         <div class="row" id="txt-img-row1">
             <div class="col">
                 <h2 style="padding-top: 10%;">Misty Samia</h2>
-                <p>A multi-identity person. Sometimes she is jungle queen (monkey), sometimes she is CSS queen
+                <p>
+                    A multi-identity person. Sometimes she is jungle queen (monkey), sometimes she is CSS queen
                     but she always holds her real identity that is - 'an impostor'.
                 </p>
             </div>
@@ -38,16 +41,19 @@
             
             <div class="col">
                 <h2 style="padding-top: 10%;">Parvez Mia</h2>
-                <p>A multi-identity person. Sometimes she is jungle queen (monkey), sometimes she is CSS queen
-                    but she always holds her real identity that is - 'an impostor'.
+                <p>
+                    FYI: I wrote this about page and IG you've got some idea about me after reading other two persons' info :3
+                    TBH: Whatever you're thinking - is wrong :( That's a good paradox, isn't it?<br>
+                    I love problem solving (I meant programming problems, not social/personal problems).
                 </p>
             </div>
         </div>
         <div class="row" id="txt-img-row1">
             <div class="col">
                 <h2 style="padding-top: 10%;">Mohosin Islam Palash</h2>
-                <p>A multi-identity person. Sometimes she is jungle queen (monkey), sometimes she is CSS queen
-                    but she always holds her real identity that is - 'an impostor'.
+                <p>
+                    He looks like a good guy <strike>but he is another impostor</strike> and he really is.
+                    He made '24x7 studying' myth true.
                 </p>
             </div>
     
